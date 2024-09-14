451884ef0c0cb0dfb356f5f2f75f181a5b3c7ebd
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2024-09-14 15:37:31 +0200

    14/9/2024

--- src/asm/vote.asm
+++ src/asm/vote.asm
@@ -1,42 +1,41 @@
-INCLUDE ./INCLUDE/Irvine32.inc
+INCLUDE Irvine32.inc
 
 .data
 str1 BYTE " Enter your name! " , 0
-fileName BYTE 20 DUP (0)
-age = 18
 str2 BYTE " Enter your age! " , 0
 str3 BYTE " Eligible " ,0
 str4 BYTE " Not Eligible ",0
+name BYTE 20 DUP (0)
+minAge = 18
 
 .code
 main PROC
+    mov edx , OFFSET str1
+    call WriteString
 
-mov edx , OFFSET str1
-call WriteString
+    mov edx, OFFSET name
+    mov ecx, SIZEOF name - 1
+    call ReadString
 
-mov edx, OFFSET fileName
-mov ecx, SIZEOF fileName - 1
-call ReadString
+    mov edx , OFFSET str2
+    call WriteString
 
-mov edx , OFFSET str2
-call WriteString
+    call ReadInt
 
-call ReadInt
+    cmp eax , minAge
+    jb NotEligible
 
-cmp eax , age
-jb L1
+    mov edx, OFFSET str3
+    call Writestring
+    jmp Done
 
-mov edx, OFFSET str3
-call Writestring
-jmp L2
+NotEligible:
+    mov edx , OFFSET str4
+    call WriteString
 
-L1: 
-mov edx , OFFSET str4
-call WriteString
+Done:
+     ;call DumpRegs
 
-L2:
- call DumpRegs
-
-exit
+    exit
 main ENDP
 END main
